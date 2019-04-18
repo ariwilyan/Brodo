66fca827e1effc175e9ae3eb6ffdc0bb52f4e66c
titanggraita <titanggraita@gmail.com>
2019-04-18 19:56:43 +0700

    view halaman utama

---
+++ application/views/halaman_utama.php
@@ -0,0 +1,207 @@
+<?php ?>
+<html>
+    <head>
+        <title>
+            BRODO 
+        </title>
+        <link rel="icon" sizes="192x192" href="assets/brodo.png">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <style>
+                .carousel-inner img {
+                    width: 100%;
+                    height: 100%;
+                }
+        </style>
+                     
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-sm navbar-light fixed-top" style="background-color:white; border-bottom: 1px solid gainsboro; padding: 5px;">
+            <ul class="nav">
+                <a href="#BRODO" style="margin-left:20px;">
+                    <img src="assets/brodo1.png" alt="BRODO" width="130" height="35">
+                </a>
+            </ul>
+            <ul class="nav navbar-nav" style="margin-left:200px;">
+                <li class="nav-item dropdown"><a class="nav-link" data-toggle="dropdown" href="#BRODO" style="color:black;">Koleksi<span class=""></span></a> 
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-item"><a class="nav-link"href="#">Best Seller</a></li>
+                        <li class="" style="border-bottom: 2px solid gainsboro;margin-left:10px;width: 135"><a class=""href="#"></a></li>
+                        <li class="dropdown-item"><a class="nav-link"href="#">Terbaru</a></li>
+                    </ul>
+                <li class="navbar h5">|</li>
+                </li>
+                <li class="nav-item dropdown-content"><a class="nav-link" data-toggle="dropdown" href="#BRODO" style="color:black; margin-left: 10px; margin-right: 10px;">Kategori<span class=""></span></a>
+                    <ul class="dropdown-menu multi-column columns-3" style="min-width: 2000px;">
+                        </button>
+                      <div class="row">
+                            <ul class="multi-column-dropdown">
+                                <li class="dropdown-header"><a class="nav-link"href="#" style="margin-left: 10px; color:black;">Sandals</a></li>
+                            </ul>
+                            <ul class="multi-column-dropdown">
+                                <li class="dropdown-header"><a class="nav-link"href="#" style="color:black;border-bottom: 2px solid gainsboro;width: 160">Aksesoris</a></li>
+                                <li class="dropdown-item"><a class="nav-link"href="#" style="border-bottom: 2px solid gainsboro;width: 160">All</a></li>
+                                <li class="dropdown-item"><a class="nav-link"href="#">Grooming</a></li>
+                            </ul>
+                            <ul class="multi-column-dropdown">
+                                <li class="dropdown-header"><a class="nav-link"href="#" style="color:black;border-bottom: 2px solid gainsboro;width: 160">Inspirasi</a></li>
+                                <li class="dropdown-item"><a class="nav-link"href="#" style="border-bottom: 2px solid gainsboro;width: 160">Outfit Kerja</a></li>
+                                <li class="dropdown-item"><a class="nav-link"href="#">Outfit Travel</a></li>
+                            </ul>
+                            <ul class="multi-column-dropdown">
+                                <li class="dropdown-header"><a class="nav-link"href="#" style="color:black;border-bottom: 2px solid gainsboro;width: 160">Shoes</a></li>
+                                <li class="dropdown-item"><a class="nav-link"href="#"style="border-bottom: 2px solid gainsboro;width: 160">Signature Collections</a></li>
+                                <li class="dropdown-item"><a class="nav-link"href="#"style="border-bottom: 2px solid gainsboro;width: 160">Sneakers</a></li>
+                                <li class="dropdown-item"><a class="nav-link"href="#">Boots</a></li>
+                            </ul>
+                            <ul class="multi-column-dropdown">
+                                    <li class="dropdown-header"><a class="nav-link"href="#" style="color:black;border-bottom: 2px solid gainsboro;width: 160">Pakaian</a></li>
+                                    <li class="dropdown-item"><a class="nav-link"href="#"style="border-bottom: 2px solid gainsboro;width: 160">Bawahan</a></li>
+                                    <li class="dropdown-item"><a class="nav-link"href="#">Atasan</a></li>
+                            </ul>
+                            
+                       </div>
+                       
+                    </ul>
+                    <li class="navbar h5">|</li>
+                    <li class="nav-item dropdown"><a class="nav-link" href="#BRODO" style="color:black; margin-left: 10px; margin-right: 10px;">Jurnal<span class=""></span></a></li>
+                    <li class="navbar h5">|</li>
+                    <li class="nav-item dropdown"><a class="nav-link "  href="#BRODO" style="color:black; margin-left: 10px; margin-right: 10px;">Tentang Kami<span class=""></span></a></li>
+                    <li class="navbar h5">|</li>
+                    <li class="nav-item dropdown"><a class="nav-link "  href="#BRODO" style="color:black; margin-left: 10px; margin-right: 10px;">Toko Kami<span class=""></span></a></li>
+
+                </li>
+            </ul>
+            <ul class="nav navbar-right">
+                <li style="margin-left:95px;">
+                    <img src="assets/search.png" alt="BRODO" width="25" height="25" style="margin-left:30px;">
+                    <img src="assets/user.png" alt="BRODO" width="25" height="25" style="margin-left:30px;" >
+                    <img src="assets/cart.png" alt="BRODO" width="25" height="25" style="margin-left:30px;">
+                </li>
+            </ul>
+            
+                
+        </nav>
+        <div id="demo" class="carousel slide" data-ride="carousel">
+
+                <!-- Indicators -->
+                <ul class="carousel-indicators">
+                  <li data-target="#demo" data-slide-to="0" class="active"></li>
+                  <li data-target="#demo" data-slide-to="1"></li>
+                  <li data-target="#demo" data-slide-to="2"></li>
+                  <li data-target="#demo" data-slide-to="3"></li>
+                  <li data-target="#demo" data-slide-to="4"></li>
+                </ul>
+                
+                <!-- The slideshow -->
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="assets/1.png" alt="" width="1100" height="500">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="assets/2.png" alt="" width="1100" height="500">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="assets/3.png" alt="" width="1100" height="500">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="assets/4.png" alt="" width="1100" height="500">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="assets/5.png" alt="" width="1100" height="500">
+                  </div>
+                </div>
+                
+                <!-- Left and right controls -->
+                <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                  <span class="carousel-control-prev-icon"></span>
+                </a>
+                <a class="carousel-control-next" href="#demo" data-slide="next">
+                  <span class="carousel-control-next-icon"></span>
+                </a>
+              </div>
+        <div class="row">
+            <div class="col-sm-4" style="margin: 20px 15px 20px 15px;margin-right: 15px;">
+                <img src="assets/grid1.PNG" width="150%" height="55%">
+            </div>
+            <div class="col-sm-4" style="margin: 20px 15px 20px 15px; margin-left: 185px;">
+                <img src="assets/grid2.PNG" width="150%" height="55%">
+            </div>
+        </div>
+        <div class="row">
+                <div class="col-sm-3" style="margin: 5px 55px 5px 5px;margin-left: 15px;">
+                    <img src="assets/grid3.PNG" width="135%" height="35%">
+                </div>
+                <div class="col-sm-3" style="margin: 5px 5px 5px 5px;margin-left: 35px;">
+                    <img src="assets/grid4.PNG" width="135%" height="35%">
+                </div>
+                <div class="col-sm-3" style="margin: 5px 5px 5px 5px; margin-left: 95px;">
+                    <img src="assets/grid5.PNG" width="135%" height="35%">
+                </div>
+        </div> 
+    </body>
+    <div class="container-fluid" id="footer" style="background-color: whitesmoke;padding: 50px; margin-top: 10px; text-align:center;">
+        <h3 class="text-bold text-center" style="font-size:14px;color: #003142;">Gak mau ketinggalan promo dari Brodo?</h3>
+        <p class="text-center" style="font-size:13px;color: #003142;;">Berlangganan sekarang!</p>
+        <div class="form-group mx-sm-3 sm-2">
+            <input type="email" class="form-md-control" id="inputemail" placeholder="Masukkan e-mail Bro/Sis">
+            <button type="button" class="btn btn-default btn-md" style="background-color:#003142; color:white;">Kirim</button>
+        </div>
+        
+    </div>
+    <div class="container-fluid">
+        <div class="row" style="background: #003142;">
+            <div class="col-md-2" style="padding-top:30px;">
+                <img src="assets/brodocoklat.png" width="120" height="45">
+            </div>
+            <div class="col-md-2" style="color:white;padding-top:30px;">
+                <p style="font-weight:bold; font-size:18px;">BRODO FOOTWEAR</p>
+                <div class="row">
+                        <div class="column" style="font-size:12px; padding-bottom: 15px;">
+                            <p>Cara Pemesanan</p>
+                            <p>Konfirmasi Pembayaran</p>
+                            <p>Pengembalian dan Penukaran</p>
+                            <p>Bantuan dan Saran</p>
+                            <p>Hubungi Kami</p>
+                            <p>Testimonial</p>
+                        </div>
+                </div>
+            </div>
+            <div class="col-md-2" style="color:white;padding-top:30px;">
+                    <p style="font-weight:bold; font-size:18px;">LEGAL</p>
+                    <div class="row">
+                            <div class="column" style="font-size:12px; padding-bottom: 15px;">
+                                <p>Aturan Penggunaan</p>
+                                <p>Kebijakan Privasi</p>
+                            </div>
+                    </div>
+                    <div class="row">
+                            <div class="column" style="font-size:12px; padding-bottom: 15px;">
+                                <p style="font-weight:bold; font-size:18px;">LAINNYA</p>
+                                <p>Jurnal</p>
+                                <p>Halaman Video</p>
+                            </div>
+                    </div>
+            </div>
+            <div class="col-md-2" style="color:white;padding-top:30px;">
+                    <p style="font-weight:bold; font-size:18px;">KONTAK BRODO</p>
+                    <div class="row">
+                            <div class="column" style="font-size:12px; padding-bottom: 15px;">
+                                <p>Customer service kami dapat dihubungi 7 hari dalam seminggu, hari kerja antara pkl. 08.00 - 21.00 & akhir pekan pkl. 10.00 - 21.00.</p>
+                                <p>Call Center :
+                                    0811 854 5555
+                                    0888 0808 5555</p>
+                                <p>Email: info@bro.do</p>
+                            </div>
+                    </div>
+            </div>
+            <div class="col-md-2" style="color:white;padding-top:30px;">
+                <img src="assets/footer.png" width="250" height="150">
+            </div>
+        </div>
+    </div>
+</html>
+    
