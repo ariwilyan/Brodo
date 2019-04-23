8a683161390ae6bbd3451f1e56e4c7646da388fa
ariwilyan <ariwilyan@gmail.com>
2019-04-23 18:38:59 +0700

    tambahan sedikit crud admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -21,20 +21,7 @@ class Admin extends CI_Controller {
 				redirect("admin/landing_brodo");
 			}
 		}
-
-		// public function index()
-		// {
-
-		// 	$data['judul'] = 'Daftar Mahasiswa';
-		// 	$data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
-		// 	if ($this->input->post('keyword')) {
-		// 		$data['mahasiswa'] = $this->Mahasiswa_model->cariDataMahasiswa();
-		// 	}
-		// 	$this->load->view('templates/header', $data);
-		// 	$this->load->view('mahasiswa/index', $data);
-		// 	$this->load->view('templates/footer');
-		// }
-
+		
     public function landing_brodo(){
 				if ($this->session->login) {
 					redirect('admin/landing_home');
